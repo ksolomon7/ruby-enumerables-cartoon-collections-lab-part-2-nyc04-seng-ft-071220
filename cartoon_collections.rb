6b42c6d023d8324f0a8936988f77a2f8871d91f8
Kalkidan Taye <kalkidan.solomon2@gmail.com>
2020-07-20 21:27:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,7 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  planeteer_calls.find do |planet|
+  planeteer_calls.include? do |planet|
       if planet == valid_calls
         puts planet
       else
