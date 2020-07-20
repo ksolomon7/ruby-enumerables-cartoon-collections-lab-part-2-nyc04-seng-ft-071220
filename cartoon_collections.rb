39a7520a3ac0ddf49904736fc29006fb756a8a3f
Kalkidan Taye <kalkidan.solomon2@gmail.com>
2020-07-20 19:52:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,4 +1,5 @@
 def square_array(array)
+  array.map{|element| element**2}
   # Use an Enumerable to square every element in the passed in array
   # Return a new array of the results
 end
@@ -15,6 +16,6 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
+  # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above
   # Return the first valid call found, or return nil if no valid calls are found
 end
