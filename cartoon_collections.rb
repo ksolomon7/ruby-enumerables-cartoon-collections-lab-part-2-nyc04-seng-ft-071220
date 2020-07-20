b89484dcb591feba56b97789bbca6f2f6ea65d0a
Kalkidan Taye <kalkidan.solomon2@gmail.com>
2020-07-20 21:14:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,11 +19,11 @@ def find_valid_calls(planeteer_calls)
   planeteer_calls.any? do |planet|
     valid_calls.count do |calls|
       if planet == calls
-        puts planet
+        planet
       else
         nil
       end
-    end  
+    end
   end
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above
   # Return the first valid call found, or return nil if no valid calls are found
