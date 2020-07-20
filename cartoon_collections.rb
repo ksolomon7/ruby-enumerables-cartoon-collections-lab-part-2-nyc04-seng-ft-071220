09ee5f6063e41098e63581b5badab89936fa2d19
Kalkidan Taye <kalkidan.solomon2@gmail.com>
2020-07-20 20:14:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(planeteer_calls)
-  planeteer_calls.count do
+  planeteer_calls.each do
     if planet.length>4
       true
     else
